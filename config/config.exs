c049f48155951272dc78a37eb0664b0178fd9b86
Tristan Sloughter <t@crashfast.com>
2020-03-29 09:51:58 -0600

    fix last cases of opencensus being used instead of opentelemtry

--- config/config.exs
+++ config/config.exs
@@ -10,11 +10,11 @@ use Mix.Config
 
 # You can configure your application as:
 #
-#     config :opencensus_ecto, key: :value
+#     config :opentelemetry_ecto, key: :value
 #
 # and access this configuration in your application as:
 #
-#     Application.get_env(:opencensus_ecto, :key)
+#     Application.get_env(:opentelemetry_ecto, :key)
 #
 # You can also configure a third-party app:
 #
